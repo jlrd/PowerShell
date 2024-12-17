a446cf4638db79bdd05b684e20554bd41b4c867a
Joel Reed <joelreed@outlook.com>
2024-12-17 06:22:07 -0700

    PowerShell profile update

--- Profiles/Kyndryl/Microsoft.PowerShell_profile.ps1
+++ Profiles/Kyndryl/Microsoft.PowerShell_profile.ps1
@@ -15,3 +15,58 @@ if (Get-Module -ListAvailable -Name PwshSpectreConsole) {
     Format-SpectreAligned -HorizontalAlignment Right -VerticalAlignment Middle
 }
 
+Function Start-Monitor
+{
+	[Cmdletbinding()]
+	Param (
+		[Parameter(Mandatory = $false)]
+		[string]$Minutes
+	)
+
+	Begin
+	{
+		Write-Verbose " [$($MyInvocation.InvocationName)] :: Start Process"
+	}
+
+	Process
+	{
+		Add-Type -AssemblyName System.Windows.Forms
+		$shell = New-Object -com "Wscript.Shell"
+
+		$pshost = Get-Host
+		$pswindow = $pshost.ui.rawui
+		$pswindow.windowtitle = 'Monitor'
+
+        if(!$minutes)
+        {
+        $Minutes = Read-Host -Prompt "Enter minutes for monitoring: "
+        }
+
+		for ($i = 0; $i -lt $Minutes; $i++)
+		{
+			Clear-Host
+			$timeleft = $Minutes - $i
+			Write-Host (Get-Date -Format HH:mm:ss) -ForegroundColor Green
+			Write-Host 'Monitor left: ' -NoNewline
+			Write-Host "$timeleft" -ForegroundColor Red -NoNewline
+			Write-Host ' Minutes'
+			$shell.sendkeys(' ')
+			for ($j = 0; $j -lt 6; $j++)
+			{
+				for ($k = 0; $k -lt 10; $k++)
+				{
+					Write-Progress -Activity 'Evaluating reality ...' -PercentComplete ($k * 10) -Status "Please ... don't disturb me."
+					Start-Sleep -Seconds 1
+				}
+			}
+			$Pos = [System.Windows.Forms.Cursor]::Position
+			$x = ($pos.X % 500) + 1
+			$y = ($pos.Y % 500) + 1
+			[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+		}
+	}
+	End
+	{
+		Write-Verbose " [$($MyInvocation.InvocationName)] :: End Process"
+	}
+}
